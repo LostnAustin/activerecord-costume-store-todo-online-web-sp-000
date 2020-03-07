bf836c9d7e2f71b1453655b01311eb350ddb0d91
Chad Lengefeld <chadlengefeld@gmail.com>
2020-03-07 19:48:22 +0000

    Automatically backed up by Learn

--- db/migrate/costumes.rb
+++ db/migrate/costumes.rb
@@ -1,11 +1,13 @@
 class CreateCostumes < ActiveRecord::Migration[5.1]
   def change
-    create_table :costumes do |t|
-      t.string :name
-      t.integer :price
-      t.integer :size
-      t.string :image_url
-    end
+    create_table :students,
+    :name
+    # create_table :costumes do |t|
+    #   t.string :name
+    #   t.integer :price
+    #   t.integer :size
+    #   t.string :image_url
+    # end
   end
 
 end
