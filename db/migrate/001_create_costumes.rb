ecba7ff6f3befc2821bb9841d467605d9f102bdc
Chad Lengefeld <chadlengefeld@gmail.com>
2020-03-07 20:03:36 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -3,7 +3,7 @@ class CreateCostumes < ActiveRecord::Migration
   def change
     create_table :costumes
       :name, :string
-  #
+  
   #     create_table :costumes do |t|
   #       t.string :name
   # end
