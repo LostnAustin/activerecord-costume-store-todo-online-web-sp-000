8e6eb274d816cf93b6a8ca650fb3b3e107a4f58b
Chad Lengefeld <chadlengefeld@gmail.com>
2020-03-07 20:21:54 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -5,6 +5,13 @@ class HauntedHouses < ActiveRecord::Migration[5.1]
   def change
     create_table :costumes do |t|
       t.string :name
+      t.string :location
+      t.string :theme
+      t.integer :price
+      t.boolean :family_friendly
+
+
+      t.string :long_description
       end
   end
 
