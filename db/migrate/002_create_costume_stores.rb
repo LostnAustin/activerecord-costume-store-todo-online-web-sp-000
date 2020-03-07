86d8f04614011dfea7d1e62a86e45c2cb012bc64
Chad Lengefeld <chadlengefeld@gmail.com>
2020-03-07 20:24:24 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -7,7 +7,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
     t.integer :costume_inventory
     t.integer :number_of_employees
     t.boolean :still_in_business
-    t.
+    t.timestamps
     end
 
 end
